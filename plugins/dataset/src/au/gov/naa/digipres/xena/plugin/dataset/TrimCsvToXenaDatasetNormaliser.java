e839d612625f808f3348c651a330a1d6e7cf2d44
toneill <terry.oneill@naa.gov.au>
2011-08-09 12:38:01 +1000

    Removed basic, dataset and postscript plugins

--- plugins/dataset/src/au/gov/naa/digipres/xena/plugin/dataset/TrimCsvToXenaDatasetNormaliser.java
+++
@@ -1,8 +0,0 @@
-package au.gov.naa.digipres.xena.plugin.dataset;
-public class TrimCsvToXenaDatasetNormaliser extends CsvToXenaDatasetNormaliser {
-	public TrimCsvToXenaDatasetNormaliser() {
-		this.setHeaderFieldDelimiter(':');
-		this.setFirstRowFieldNames(true);
-		this.setOneFieldHeader(true);
-	}
-}
