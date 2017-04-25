e37b5b7ff3b5653a76b34c68d83f8562e9835ba5
ricardo cole <colero0@yahoo.com>
2017-04-26 00:07:59 +1000

    removed the jpedal_lgpl as a external project. The source file has issues. Adding the binary as a maven file install or as a project repo.

--- ext/jpedal_lgpl/src/main/java/org/jpedal/external/JPedalActionHandler.java
+++
@@ -1,8 +0,0 @@
-package org.jpedal.external;
-
-import org.jpedal.examples.simpleviewer.Commands;
-import org.jpedal.examples.simpleviewer.gui.SwingGUI;
-
-public interface JPedalActionHandler {
-	public void actionPerformed(SwingGUI currentGUI, Commands commands);
-}
